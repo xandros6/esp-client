4c2a44a71b07f670af044043d173be400e2b6aba
Xandros <sandro.salari@gmail.com>
2015-11-06 13:48:36 +0100

    Integrate configuration properties files (hibernate e runtime)

--- src/main/java/org/esp/server/GuiceContextListener.java
+++ src/main/java/org/esp/server/GuiceContextListener.java
@@ -1,7 +1,12 @@
 package org.esp.server;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.esp.publisher.ui.AppUI;
 import org.esp.publisher.ui.ViewModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.vaadin.addons.guice.uiscope.UIScopeModule;
 
 import com.google.inject.Guice;
@@ -10,8 +15,19 @@ import com.google.inject.persist.jpa.JpaPersistModule;
 import com.google.inject.servlet.GuiceServletContextListener;
 
 public class GuiceContextListener extends GuiceServletContextListener {
-	@Override
-	protected Injector getInjector() {
-		return Guice.createInjector(new ESPServletModule(), new ViewModule(), new UIScopeModule(AppUI.class), new JpaPersistModule("esp-domain"));
-	}
+    
+    private static Logger logger = LoggerFactory.getLogger(GuiceContextListener.class);
+    
+    @Override
+    protected Injector getInjector() {
+        JpaPersistModule pm = new JpaPersistModule("esp-domain");
+        try {
+            Properties properties = new Properties();
+            properties.load(this.getClass().getClassLoader().getResourceAsStream("runtime.properties"));
+            pm.properties(properties);
+        } catch (IOException e) {
+            logger.error(e.getMessage(),e);
+        }       
+        return Guice.createInjector(new ESPServletModule(), new ViewModule(), new UIScopeModule(AppUI.class), pm);
+    }
 }
